938a70190f2330da3f60a347e1981a846d33f5d9
Suze Zhang <51674917+sz247@users.noreply.github.com>
2019-07-12 00:36:03 -0400

    Create no771.cpp

---
+++ no771.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int i, j, count = 0;
+        for(i=0; i<J.length(); i++){
+            for (j=0; j<S.length(); j++){
+                if (J[i] == S[j]){
+                    count = count + 1; 
+                }
+            }
+        }
+        return count; 
+    }
+};
